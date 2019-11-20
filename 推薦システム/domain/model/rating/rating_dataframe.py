c142e0ef229e9e2b9b4724d3bc50bed277f602bf
gtaiyou24 <gtaiyou24@gmail.com>
2019-11-20 18:53:18 +0900

    update 推薦システム

--- 推薦システム/domain/model/rating/rating_dataframe.py
+++ 推薦システム/domain/model/rating/rating_dataframe.py
@@ -1,7 +1,8 @@
 import numpy as np
+
 import pandas as pd
 
 
 class RatingDataFrame(pd.DataFrame):
     def __init__(self, rating_matrix: np.array, user_id_list: list, item_id_list: list):
-        super().__init__(rating_matrix, index=user_id, columns=item_id_list)
+        super().__init__(rating_matrix, index=user_id_list, columns=item_id_list)
